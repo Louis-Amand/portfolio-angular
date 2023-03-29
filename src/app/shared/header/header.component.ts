d8213c0e91bdb647caa3faaf663ad20e6ca81c29
Louis-Amand <48965957+Louis-Amand@users.noreply.github.com>
2023-03-29 17:22:29 +0200

    updating navbar with new items and new scss

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { isNgContainer } from '@angular/compiler';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit {
       items: MenuItem[] = [];
@@ -12,94 +14,34 @@ export class HeaderComponent implements OnInit {
       ngOnInit() {
           this.items = [
               {
-                  label: 'File',
+                  label: 'Curriculum Vitae',
                   icon: 'pi pi-fw pi-file',
                   items: [
-                      {
-                          label: 'New',
-                          icon: 'pi pi-fw pi-plus',
-                          items: [
-                              {
-                                  label: 'Bookmark',
-                                  icon: 'pi pi-fw pi-bookmark'
-                              },
-                              {
-                                  label: 'Video',
-                                  icon: 'pi pi-fw pi-video'
-                              }
-                          ]
-                      },
-                      {
-                          label: 'Delete',
-                          icon: 'pi pi-fw pi-trash'
-                      },
-                      {
-                          separator: true
-                      },
-                      {
-                          label: 'Export',
-                          icon: 'pi pi-fw pi-external-link'
-                      }
-                  ]
+                    {
+                        label: 'Qui suis-je ?',
+                        icon: 'pi pi-fw pi-pencil',
+                      
+                    },
+                    {
+                        label: 'Mes compétences',
+                        icon: 'pi pi-fw pi-pencil',
+                      
+                    },
+                  
+                ],
+
               },
               {
-                  label: 'Edit',
-                  icon: 'pi pi-fw pi-pencil',
-                  items: [
-                      {
-                          label: 'Left',
-                          icon: 'pi pi-fw pi-align-left'
-                      },
-                      {
-                          label: 'Right',
-                          icon: 'pi pi-fw pi-align-right'
-                      },
-                      {
-                          label: 'Center',
-                          icon: 'pi pi-fw pi-align-center'
-                      },
-                      {
-                          label: 'Justify',
-                          icon: 'pi pi-fw pi-align-justify'
-                      }
-                  ]
+                label: 'Mes projets',
+                icon: 'pi pi-fw pi-user',
+            },
+                   
+              {
+                label: 'Mes rédactions',
+                icon: 'pi pi-fw pi-calendar',
               },
               {
-                  label: 'Users',
-                  icon: 'pi pi-fw pi-user',
-                  items: [
-                      {
-                          label: 'New',
-                          icon: 'pi pi-fw pi-user-plus'
-                      },
-                      {
-                          label: 'Delete',
-                          icon: 'pi pi-fw pi-user-minus'
-                      },
-                      {
-                          label: 'Search',
-                          icon: 'pi pi-fw pi-users',
-                          items: [
-                              {
-                                  label: 'Filter',
-                                  icon: 'pi pi-fw pi-filter',
-                                  items: [
-                                      {
-                                          label: 'Print',
-                                          icon: 'pi pi-fw pi-print'
-                                      }
-                                  ]
-                              },
-                              {
-                                  icon: 'pi pi-fw pi-bars',
-                                  label: 'List'
-                              }
-                          ]
-                      }
-                  ]
-              },
-              {
-                  label: 'Events',
+                  label: 'Me contacter',
                   icon: 'pi pi-fw pi-calendar',
                   items: [
                       {
@@ -116,21 +58,11 @@ export class HeaderComponent implements OnInit {
                               }
                           ]
                       },
-                      {
-                          label: 'Archieve',
-                          icon: 'pi pi-fw pi-calendar-times',
-                          items: [
-                              {
-                                  label: 'Remove',
-                                  icon: 'pi pi-fw pi-calendar-minus'
-                              }
-                          ]
-                      }
                   ]
               },
               {
-                  label: 'Quit',
-                  icon: 'pi pi-fw pi-power-off'
+                label:"",
+                icon:"pi pi-user"
               }
           ];
       }

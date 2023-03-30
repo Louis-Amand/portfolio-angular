c6d5adacfaa8a29be09ba8ba867610accca99d19
Louis-Amand <48965957+Louis-Amand@users.noreply.github.com>
2023-03-30 22:33:39 +0200

    adding translations and params like dislexia...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { ArticlesListComponent } from './articles/articles-list/articles-list.co
 import { HomePageComponent } from './home-page/home-page/home-page.component';
 import { ContactPageComponent } from './contact-us/contact-page/contact-page.component';
 import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
+// import ngx-translate and the http loader
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -22,14 +26,28 @@ import { ProjectsListComponent } from './projects/projects-list/projects-list.co
     HomePageComponent,
     ContactPageComponent,
     ProjectsListComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
-    MenubarModule
+    MenubarModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}

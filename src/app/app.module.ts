6a8f83611c1ed72573c697186ab108ea7108921e
Louis-Amand <48965957+Louis-Amand@users.noreply.github.com>
2023-04-01 17:08:10 +0200

    adding bg front-page and about me section with translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,31 +7,38 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { DividerModule } from 'primeng/divider';
 import { ArticlesListComponent } from './articles/articles-list/articles-list.component';
 import { HomePageComponent } from './home-page/home-page/home-page.component';
 import { ContactPageComponent } from './contact-us/contact-page/contact-page.component';
 import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
+
 // import ngx-translate and the http loader
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BackgroundComponent } from './home-page/background/background.component';
+import { AboutMeComponent } from './home-page/about-me/about-me.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
+
     FooterComponent,
     HeaderComponent,
     ArticlesListComponent,
     HomePageComponent,
     ContactPageComponent,
     ProjectsListComponent,
-
+    BackgroundComponent,
+    AboutMeComponent,
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
+    DividerModule,
     MenubarModule,
     HttpClientModule,
     TranslateModule.forRoot({
